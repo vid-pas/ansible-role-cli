5ea14dab9f0021a15ba410b97f42571db6447140
Roderik van der Veer <roderik@settlemint.com>
2019-06-12 15:37:33 +0200

    $(kube_ps1)

--- files/bashrc.prompt.sh
+++ files/bashrc.prompt.sh
@@ -5,8 +5,10 @@ export GIT_PS1_SHOWUNTRACKEDFILES=true
 export GIT_PS1_SHOWSTASHSTATE=true
 export PROMPT_DIRTRIM=3
 
+source "/usr/local/opt/kube-ps1/share/kube-ps1.sh"
+
 if [[ ${EUID} == 0 ]] ; then
     PS1='\[\033[0;33m\][\[\033[0;31m\]\u\[\033[0;31m\]@\[\033[0;31m\]\h \[\033[1;32m\]\w\[\033[0;33m\]]\[\033[0;31m\]$(declare -F __git_ps1 &>/dev/null && __git_ps1 "(%s)")\[\033[0;37m\]\\$ \[\033[00m\]'
 else
-    PS1='\[\033[0;33m\][\[\033[0;36m\]\u\[\033[0;36m\]@\[\033[0;36m\]\h \[\033[0;32m\]\w\[\033[0;33m\]]\[\033[0;31m\]$(declare -F __git_ps1 &>/dev/null && __git_ps1 "(%s)")\[\033[0;37m\]\\$ \[\033[00m\]'
+    PS1='\[\033[0;33m\][\[\033[0;36m\]\u\[\033[0;36m\]@\[\033[0;36m\]\h \[\033[0;32m\]\w\[\033[0;33m\]]\[\033[0;31m\]$(declare -F __git_ps1 &>/dev/null && __git_ps1 "(%s)")$(kube_ps1)\[\033[0;37m\]\\$ \[\033[00m\]'
 fi
