555ff6d3ec9a8f07bdf0c4d4dedc25bf1e2b7bb4
Roderik van der Veer <roderik@settlemint.com>
2019-06-12 17:15:47 +0200

    Update bashrc.prompt.sh

--- files/bashrc.prompt.sh
+++ files/bashrc.prompt.sh
@@ -8,6 +8,8 @@ export GIT_PS1_SHOWCOLORHINTS=true
 export GIT_PS1_HIDE_IF_PWD_IGNORED=true
 export PROMPT_DIRTRIM=3
 
+export KUBE_PS1_PREFIX=" ("
+export KUBE_PS1_SYMBOL_ENABLE=false
 source "/usr/local/opt/kube-ps1/share/kube-ps1.sh"
 
 PROMPT_COMMAND='__git_ps1 "\[\033[0;33m\][\[\033[0;31m\]\u\[\033[0;31m\]@\[\033[0;31m\]\h \[\033[1;32m\]\w\[\033[0;33m\]]\[\033[00m\]$(kube_ps1)" "\[\033[0;37m\]\\$ \[\033[00m\]"'
