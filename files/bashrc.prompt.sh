1c0352760601462c9629a2bf6b156501948877c3
Roderik van der Veer <roderik@settlemint.com>
2019-06-12 16:14:14 +0200

    Update bashrc.prompt.sh

--- files/bashrc.prompt.sh
+++ files/bashrc.prompt.sh
@@ -5,7 +5,9 @@ export GIT_PS1_SHOWUNTRACKEDFILES=true
 export GIT_PS1_SHOWSTASHSTATE=true
 export PROMPT_DIRTRIM=3
 
-source "/usr/local/opt/kube-ps1/share/kube-ps1.sh"
+if [ -f "/usr/local/opt/kube-ps1/share/kube-ps1.sh" ]; then
+    source "/usr/local/opt/kube-ps1/share/kube-ps1.sh"
+fi
 
 if [[ ${EUID} == 0 ]] ; then
     PS1='\[\033[0;33m\][\[\033[0;31m\]\u\[\033[0;31m\]@\[\033[0;31m\]\h \[\033[1;32m\]\w\[\033[0;33m\]]\[\033[0;31m\]$(declare -F __git_ps1 &>/dev/null && __git_ps1 "(%s)")\[\033[0;37m\]\\$ \[\033[00m\]'
